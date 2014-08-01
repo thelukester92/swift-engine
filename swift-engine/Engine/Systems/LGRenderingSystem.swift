6eafe8d79031b630a6d9aa9e2df896337f3eb187
thelukester92 <thelukester92@me.com>
2014-08-01 09:18:23 -0500

    Only pull sprite texture when position changed

--- swift-engine/Engine/Systems/LGRenderingSystem.swift
+++ swift-engine/Engine/Systems/LGRenderingSystem.swift
@@ -13,6 +13,7 @@ class LGRenderingSystem: LGSystem
 	var scene: LGScene
 	final var positions	= [LGPosition]()
 	final var sprites	= [LGSprite]()
+	final var posForId	= [Int:Int]()
 	
 	init(scene: LGScene)
 	{
@@ -33,7 +34,46 @@ class LGRenderingSystem: LGSystem
 		
 		let position	= entity.get(LGPosition)!
 		let sprite		= entity.get(LGSprite)!
-		var pos			= 1
+		
+		prepareSprite(sprite)
+		
+		positions += position
+		sprites += sprite
+	}
+	
+	override func update()
+	{
+		// TODO: Don't render everything every time... only render moved/changed things
+		for id in 0 ..< entities.count
+		{
+			let sprite = sprites[id]
+			let position = positions[id]
+			
+			if sprite.isVisible
+			{
+				updateSpriteFrame(sprite)
+				
+				// Update sprite position, orientation, and other SKSpriteNode properties
+				
+				sprite.node.position.x	= CGFloat(position.x + sprite.offset.x + Double(sprite.node.size.width / 2))
+				sprite.node.position.y	= CGFloat(position.y + sprite.offset.y + Double(sprite.node.size.height / 2))
+				sprite.node.xScale		= CGFloat(sprite.scale.x)
+				sprite.node.yScale		= CGFloat(sprite.scale.y)
+				sprite.node.zRotation	= CGFloat(sprite.rotation)
+				sprite.node.zPosition	= CGFloat(sprite.layer)
+				sprite.node.alpha		= CGFloat(sprite.opacity)
+				sprite.node.hidden		= false
+			}
+			else
+			{
+				sprite.node.hidden = true
+			}
+		}
+	}
+	
+	func prepareSprite(sprite: LGSprite)
+	{
+		var pos = 1
 		
 		var node: SKSpriteNode!
 		if let _ = sprite.node
@@ -65,64 +105,41 @@ class LGRenderingSystem: LGSystem
 			sprite.node.color = UIColor.whiteColor()
 			sprite.node.size = CGSize(width: CGFloat(body.width), height: CGFloat(body.height))
 		}
-		
-		positions += position
-		sprites += sprite
 	}
 	
-	override func update()
+	func updateSpriteFrame(sprite: LGSprite)
 	{
-		// TODO: Don't render everything every time... only render moved/changed things
-		for id in 0 ..< entities.count
+		if let state = sprite.currentState
 		{
-			let sprite = sprites[id]
-			let position = positions[id]
-			
-			if sprite.isVisible
+			if !state.isAnimated || ++state.counter > state.duration
 			{
-				// Update sprite frame
+				// Animated animatable sprites
 				
-				if let state = sprite.currentState
+				if state.isAnimated
 				{
-					// TODO: Don't make it fetch a new texture using textureAtPosition every time
-					
-					if !state.isAnimated || ++state.counter > state.duration
+					// TODO: use a state.incrementor property (instead of position++), so some sprites can run faster/slower
+					state.position++
+					if state.position > state.end
 					{
-						sprite.node.texture = sprite.spriteSheet?.textureAtPosition(state.position)
-						
-						if state.isAnimated
+						if state.loops
 						{
-							state.position++
-							if state.position > state.end
-							{
-								if state.loops
-								{
-									state.position = state.start
-								}
-								else
-								{
-									state.position = state.end
-								}
-							}
-							state.counter = 0
+							state.position = state.start
+						}
+						else
+						{
+							state.position = state.end
 						}
 					}
+					state.counter = 0
 				}
 				
-				// Update sprite position, orientation, and other SKSpriteNode properties
+				// Only pull a new texture if the position changed
 				
-				sprite.node.position.x	= CGFloat(position.x + sprite.offset.x + Double(sprite.node.size.width / 2))
-				sprite.node.position.y	= CGFloat(position.y + sprite.offset.y + Double(sprite.node.size.height / 2))
-				sprite.node.xScale		= CGFloat(sprite.scale.x)
-				sprite.node.yScale		= CGFloat(sprite.scale.y)
-				sprite.node.zRotation	= CGFloat(sprite.rotation)
-				sprite.node.zPosition	= CGFloat(sprite.layer)
-				sprite.node.alpha		= CGFloat(sprite.opacity)
-				sprite.node.hidden		= false
-			}
-			else
-			{
-				sprite.node.hidden = true
+				if !posForId[id] || posForId[id] != state.position
+				{
+					sprite.node.texture = sprite.spriteSheet?.textureAtPosition(state.position)
+					posForId[id] = state.position
+				}
 			}
 		}
 	}
