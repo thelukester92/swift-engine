889354c66c2739d0dbc425769298c831875adac3
Luke Godfrey <thelukester92@me.com>
2014-08-05 23:42:01 -0500

    Updated copyright notice.

--- swift-engine/Bridging-Header.h
+++ swift-engine/Bridging-Header.h
@@ -3,5 +3,5 @@
 //  swift-engine
 //
 //  Created by Luke Godfrey on 7/7/14.
-//  Copyright (c) 2014 Luke Godfrey. All rights reserved.
+//  Copyright (c) 2014 Luke Godfrey. See LICENSE.
 //
--- swift-engine/Game/Systems/PlayerInputSystem.swift
+++ swift-engine/Game/Systems/PlayerInputSystem.swift
@@ -3,7 +3,7 @@
 //  swift-engine
 //
 //  Created by Luke Godfrey on 7/28/14.
-//  Copyright (c) 2014 Luke Godfrey. All rights reserved.
+//  Copyright (c) 2014 Luke Godfrey. See LICENSE.
 //
 
 import UIKit
