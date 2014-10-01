4ba4092d8ee160dcff9ca056f81987b61ec888b7
thelukester92 <thelukester92@me.com>
2014-10-01 09:16:26 -0500

    Added Lua files.

---
+++ Lua/Lua.h
@@ -0,0 +1,13 @@
+//
+//  Lua.h
+//  Lua
+//
+//  Created by Luke Godfrey on 10/1/14.
+//  Copyright (c) 2014 Luke Godfrey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Lua : NSObject
+
+@end
---
+++ Lua/Lua.m
@@ -0,0 +1,13 @@
+//
+//  Lua.m
+//  Lua
+//
+//  Created by Luke Godfrey on 10/1/14.
+//  Copyright (c) 2014 Luke Godfrey. All rights reserved.
+//
+
+#import "Lua.h"
+
+@implementation Lua
+
+@end
